cadb0c1562d29a67c499e0ba0adc1ab2a2882278
Meet Khanuja <meetkhanuja22@gmail.com>
2024-04-02 14:47:39 +0530

    project completed

---
+++ backend/app.js
@@ -0,0 +1,42 @@
+import express from "express";
+import { dbConnection } from "./database/dbConnection.js";
+import jobRouter from "./routes/jobRoutes.js";
+import userRouter from "./routes/userRoutes.js";
+import applicationRouter from "./routes/applicationRoutes.js";
+import { config } from "dotenv";
+import cors from "cors";
+import { errorMiddleware } from "./middlewares/error.js";
+import cookieParser from "cookie-parser";
+import fileUpload from "express-fileupload";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+config({ path: "./config/config.env" });
+app.use(
+  cors({
+    origin: 'http://localhost:5173',
+    methods: ["GET", "POST", "DELETE", "PUT"],
+    credentials: true,
+  })
+);
+
+
+
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/tmp/",
+  })
+);
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/job", jobRouter);
+app.use("/api/v1/application", applicationRouter);
+dbConnection();
+
+app.use(errorMiddleware);
+
+
+export default app;
